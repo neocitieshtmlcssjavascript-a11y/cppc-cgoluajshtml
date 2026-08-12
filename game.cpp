0316ed8c144c34986b0247f82975225c6d512c0e
oldgevdev <neocitieshtmlcssjavascript@gmail.com>
2026-08-12 21:03:29 +0600

    Create game.cpp

---
+++ game.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "========================================" << std::endl;
+    std::cout << "  Engine cppc+cgoluajshtml is STARTING!  " << std::endl;
+    std::cout << "========================================" << std::endl;
+    std::cout << "[Step 1] Core written in C++ initialized successfully." << std::endl;
+    
+    // Сюда мы дальше подключим Go и остальные языки
+    return 0;
+}
